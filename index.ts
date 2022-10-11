5fb636300aa282b6e033bc32c9ecff2bf4da82a7
revival0728 <69998786+revival0728@users.noreply.github.com>
2022-10-11 11:44:51 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const express = require('express')
 const fs = require('fs')
 const sr = require('simple-random')
 const app = express()
-const port = 80
+const port = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
